802b371026b7fa1426050d4473320506130de942
Saptadip Santra <saptadipsantra19@gmail.com>
2026-08-20 21:48:03 +0530

    server

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
